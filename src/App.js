95773b9037978b7ae1162e2c3ce39e1432cc736f
MaisDroubi <mais.droubi@gmail.com>
2021-02-02 14:24:50 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Pokemon from './components/Pokemon'
+
+function App() {
+
+  return (
+    <div className="container">
+      <Pokemon></Pokemon>
+    </div>
+  );
+}
+
+export default App;
+
